425f47a1aec32f54d59d863c7462312b107f3e42
Ozi <ozisadiqkasai@gmail.com>
2025-01-27 20:45:47 +0100

    message

--- client/src/AboutUs.jsx
+++ client/src/AboutUs.jsx
@@ -20,16 +20,16 @@ const Wrapper = styled.div`
   position: relative;
   z-index: 3;
   margin-top: 76vh;
-  @media${({ theme }) => theme.device.mobile} {
+  @media ${({ theme }) => theme.device.mobile} {
     margin-top: 41vh;
   }
-  @media${({ theme }) => theme.device.tablet} {
+  @media ${({ theme }) => theme.device.tablet} {
     margin-top: 78vh;
   }
-  @media${({ theme }) => theme.device.laptop} {
+  @media ${({ theme }) => theme.device.laptop} {
     margin-top: 82vh;
   }
-  @media${({ theme }) => theme.device.desktop} {
+  @media ${({ theme }) => theme.device.desktop} {
     margin-top: 85vh;
   }
 `;
@@ -37,7 +37,7 @@ const Container = styled.div`
   width: 87%;
   margin: 0 auto;
   padding: 1rem 0;
-  @media${({ theme }) => theme.device.mobile} {
+  @media ${({ theme }) => theme.device.mobile} {
     width: 100%;
   }
   h2 {
@@ -49,7 +49,7 @@ const Container = styled.div`
     text-align: center;
     margin: 0 auto;
     max-width: 100%;
-    @media${({ theme }) => theme.device.mobile} {
+    @media ${({ theme }) => theme.device.mobile} {
       max-width: 95%;
       font-size:1rem;
     }
